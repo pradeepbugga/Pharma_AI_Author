c07013394d9efa4a92728bd54096fa3d98700ed7
Pradeep Bugga <pradeep.bugga0@gmail.com>
2026-04-01 17:06:07 -0400

    moved to backend folder, added comments

---
+++ backend/src/extraction/fields/extract_formula.py
@@ -0,0 +1,41 @@
+import pubchempy as pcp
+
+
+# this function extracts structural formula using PubChemPy
+# we will get the molecular formula, SMILES, and InChI if CID is available.
+
+def extract_structural_formula(drug, modality, pubchem_info):
+    if modality != "small_molecule":
+        return {
+            "value": {
+
+            "molecular_formula": None,
+            "smiles": None,
+            "inchi": None},  
+            "status": "not_applicable"
+
+        }
+
+
+    if pubchem_info.get("compound") and pubchem_info.get("cid"):
+
+        # Fetch compound info from PubChem
+        cid = pubchem_info["cid"]
+        pubchem_data = pcp.Compound.from_cid(cid).to_dict()
+      
+        return {
+            "value": {
+            "molecular_formula": pubchem_data["molecular_formula"],
+            "smiles": pubchem_data["smiles"],
+            "inchi": pubchem_data["inchi"]},
+            "status": "present",
+            "source": "pubchem"
+        }
+
+    return {
+        "value": {
+        "molecular_formula": None,
+        "smiles": None,
+        "inchi": None},
+        "status": "missing"
+    }
