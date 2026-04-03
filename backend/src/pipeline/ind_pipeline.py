bda9ccf1c7719360208eea76e3210bf5fd1d588d
Pradeep Bugga <pradeep.bugga0@gmail.com>
2026-04-03 02:44:58 -0400

    fix  ind synthesis to split based on has_clinical

--- backend/src/pipeline/ind_pipeline.py
+++ backend/src/pipeline/ind_pipeline.py
@@ -1,6 +1,6 @@
 import json
 
-from backend.src.synthesize.synthesize_ind_intro import synthesize_intro
+from backend.src.synthesize.synthesize_ind_intro import synthesize_preclinical, synthesize_clinical
 
 from backend.src.ingest.section_parser import parse_sections
 from backend.src.pipeline.main_pipeline import run_pipeline
@@ -12,11 +12,7 @@ from backend.src.synthesize.ind_fields import build_ind_fields
 PAPER_PATH = "./backend/src/data/papers/PMID_36216931/raw_text.json"
 ENTITY_COUNTS_PATH = "./extracted_entities.json"
 
-def run_ind_pipeline(entity_counts_path, paper_path):
-
-    # load entities 
-    with open(entity_counts_path) as f:
-        entity_counts = json.load(f)
+def run_ind_pipeline(entity_counts, paper_path):
 
     entities = list(entity_counts.keys())
 
@@ -61,12 +57,29 @@ def run_ind_pipeline(entity_counts_path, paper_path):
 
     fields = build_ind_fields(ind_context)
 
+
     print(json.dumps(fields, indent=2))
     # ----- SYNTHEIZE IND-INTRO -----
 
     print("\n=== SYNTHESIZED IND-INTRO STATEMENT ===")
-    ind_intro = synthesize_intro(fields, study_info.get("study_model", {}).get("model"))
+
+    has_human_data = study_info.get("administration", {}).get("clinical", {}).get("value", False) 
+     
+    print(f"Has human data: {has_human_data}")
+
+    ind_study_context = {
+        "model_type": study_info.get("study_model", {}).get("model_type"),
+        "models": study_info.get("study_model", {}).get("components", []),
+        "has_clinical": has_human_data,
+    }
+
+    if has_human_data:
+        ind_intro = synthesize_preclinical(fields, ind_study_context)
+    else:
+        ind_intro = synthesize_clinical(fields, ind_study_context)
     print(ind_intro)
    
+    return fields, ind_intro, has_human_data
+
 if __name__ == "__main__":
     run_ind_pipeline(ENTITY_COUNTS_PATH, PAPER_PATH)
