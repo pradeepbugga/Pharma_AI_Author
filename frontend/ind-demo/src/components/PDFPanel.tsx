9695b9819f2000cbf903d19bbe24c6e7f358518b
Pradeep Bugga <pradeep.bugga0@gmail.com>
2026-04-04 18:31:32 -0400

    added title

--- frontend/ind-demo/src/components/PDFPanel.tsx
+++ frontend/ind-demo/src/components/PDFPanel.tsx
@@ -1,10 +1,20 @@
 export default function PDFPanel() {
   return (
-    <div className="pdf-container">
-       <iframe
+    <div className="pdf-panel">
+
+      {/* Panel title */}
+      <div className="panel-header">
+        Preclinical KRAS Drug Paper
+      </div>
+
+      {/* PDF */}
+      <div className="pdf-container">
+        <iframe
           src="/paper.pdf"
           className="pdf-frame"
         />
+      </div>
+
     </div>
   );
 }
